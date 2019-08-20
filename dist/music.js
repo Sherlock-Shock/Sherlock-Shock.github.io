b712d2bc187a6a5e7f5744fb1cc807a68bae27e0
““Sherlock-Shock#git config --global user.name “Sherlock-Shock <752750447@qq.com>
2019-08-20 15:50:27 +0800

    Site updated: 2019-08-20 15:50:24

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "Epilogue",
-        artist: 'Justin Hurwitz',
-        url: 'https://m7.music.126.net/20190820151555/8ab4749a483985b60b3a9e5df5cf6f1d/ymusic/ac4d/87b5/676a/fde5361494dbc25f28cb24146150bae8.mp3',
-        cover: '/images/music/Epilogue.png',
+        name: "Grand Blue",
+        artist: '湘南乃風',
+        url: 'https://music.163.com/song/media/outer/url?id=1306000879.mp3',
+        cover: '/images/music/bilan.png',
       }
     ]
 });
