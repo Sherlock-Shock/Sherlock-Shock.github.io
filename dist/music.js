6856e4735a8710c306baec2e10990f348d99af45
““Sherlock-Shock#git config --global user.name “Sherlock-Shock <752750447@qq.com>
2019-08-22 18:03:58 +0800

    Site updated: 2019-08-22 18:03:50

--- dist/music.js
+++ dist/music.js
@@ -3,11 +3,25 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+	 {
+        name: 'Another Day Of Sun (From "La La Land" Soundtrack)',
+        artist: 'La La Land Cast',
+        url: 'https://music.163.com/song/media/outer/url?id=445867332.mp3',
+        cover: '/images/music/lalaland.png',
+      },
+	  {
+        name: "Sound of Silence",
+        artist: 'Paul Simon',
+        url: 'https://music.163.com/song/media/outer/url?id=1821488.mp3',
+        cover: '/images/music/sound.png',
+      },
       {
         name: "Grand Blue",
         artist: '湘南乃風',
         url: 'https://music.163.com/song/media/outer/url?id=1306000879.mp3',
         cover: '/images/music/bilan.png',
       }
+	  
+	  
     ]
 });
